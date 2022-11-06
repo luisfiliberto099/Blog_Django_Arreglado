b2fb658d69d57ba080583a433b1c8a23fa40ab75
Fili <luisfiliberto099@gmail.com>
2022-11-06 17:58:22 +0100

    se añadio la opcion de importacion y exportacion

--- aplicaciones/blog/admin.py
+++ aplicaciones/blog/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
 from aplicaciones.blog.models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-class CategoriaAdmin(admin.ModelAdmin):
+class CategoriaResouls(resources.ModelResource):
+    class Meta:
+        model = Categoria
+
+class CategoriaAdmin(ImportExportModelAdmin , admin.ModelAdmin):
     search_fields = ['nombre']
     list_display = ('nombre','estado','fecha_creacion')
+    resource_class = CategoriaResouls
 
-class AutorAdmin(admin.ModelAdmin):
+class AutorAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ['nombre', 'apellidos']
     list_display = ('nombre', 'apellidos', 'estado', 'fecha_creacion')
+    resource_class = CategoriaResouls
+
+class PostAdmin(ImportExportModelAdmin , admin.ModelAdmin):
+    resource_class = CategoriaResouls
 
 admin.site.register(Categoria, CategoriaAdmin)
 admin.site.register(Autor, AutorAdmin)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
